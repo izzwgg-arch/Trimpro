db6fa3befe9d45581eca0fe9360cb49bd0d41d86
izzwgg-arch <izzwgg-arch@users.noreply.github.com>
2026-01-25 17:22:14 -0500

    Add PM2 ecosystem config file

---
+++ ecosystem.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  apps: [
+    {
+      name: 'trimpro',
+      script: 'npm',
+      args: 'start',
+      cwd: '/root/apps/trimpro',
+      env_file: '.env',
+      env: {
+        NODE_ENV: 'production',
+        PORT: 3000,
+        HOSTNAME: '0.0.0.0',
+      },
+      instances: 1,
+      exec_mode: 'fork',
+      watch: false,
+      max_memory_restart: '1G',
+      error_file: './logs/err.log',
+      out_file: './logs/out.log',
+      log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
+      merge_logs: true,
+    },
+  ],
+}
